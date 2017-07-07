b9e93ac2dff434ab74b2d2b9820a6d1776884e8a
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2017-07-07 15:37:49 +0800

    update reserved words

--- src/Main/reserved.h
+++ src/Main/reserved.h
@@ -5,12 +5,13 @@
 #ifndef RESERVED_H_
 #define RESERVED_H_
 
-#define RESERVED_WORD_COUNT 8
+#define RESERVED_WORD_COUNT 9
 
 static const char *RESERVED_WORDS[RESERVED_WORD_COUNT] = {
     "p", //print
     "l", //loop
-    "f", //function
+    "fd", //function
+    "df", //end of function declaration
     "i", //include
     "if", //if
     "fi", //end of if
