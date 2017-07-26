62e678f5476d8aeeb757cf6fcaebbed9f08ec731
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2017-07-27 07:45:32 +0800

    interactive shell interface

--- src/Main/interactive.c
+++ src/Main/interactive.c
@@ -2,8 +2,23 @@
  *
  *  Copyright (C) 2017  Ferdinand Silva
  */
+#include <stdio.h>
+#include <string.h>
 #include "interactive.h"
+#include "info.h"
+#include "util.h"
 
 void interactive_shell() {
+    char userInput[TITIK_CHAR_PER_LINE];
+    char indicator[TITIK_CHAR_PER_LINE];
 
+    strcpy(indicator, ">>>");
+
+    printf("%s %s\n", TITIK_APP_NAME, TITIK_STRING_VERSION);
+    printf("To exit, press ^C\n");
+
+    while(T) {
+        printf("%s ", indicator);
+        fgets(userInput, TITIK_CHAR_PER_LINE, stdin);
+    }
 }
