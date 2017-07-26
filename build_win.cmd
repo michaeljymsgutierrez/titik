435367fe5aa2ab00808e6ef24105d6a71ccf9369
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2017-07-26 19:46:33 +0800

    update windows script

--- build_win.cmd
+++ build_win.cmd
@@ -1,5 +1,1 @@
-del /f src\Main\info.c
-
-copy src\Main\info_win.c src\Main\info.c
-
 cl /EHsc src/Main/interactive.c src/Main/functions.c src/Main/debug.c src/Main/reserved.c src/Main/info.c src/Main/util.c src/Main/lexer.c src/Main/parser.c src/Main/titik.c /link /out:build/titik.exe
