39dad9da46d67f658d055c0175fa972bb35e042d
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2017-07-26 13:45:08 +0800

    add source files for interactive shell

---
+++ src/Main/interactive.c
@@ -0,0 +1,5 @@
+/* interactive.c
+ *
+ *  Copyright (C) 2017  Ferdinand Silva
+ */
+#include "interactive.h"
---
+++ src/Main/interactive.h
@@ -0,0 +1,8 @@
+/* interactive.h
+ *
+ *  Copyright (C) 2017  Ferdinand Silva
+ */
+#ifndef INTERACTIVE_H_
+#define INTERACTIVE_H_
+
+#endif
