46be3e0d949419f1e2ce2500f29049bca3fd7fd1
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2017-07-27 08:47:18 +0800

    update interactive shell ;

--- src/Main/interactive.c
+++ src/Main/interactive.c
@@ -9,6 +9,7 @@
 #include "util.h"
 #include "lexer.h"
 #include "debug.h"
+#include "parser.h"
 
 void interactive_shell() {
     char userInput[TITIK_CHAR_PER_LINE];
@@ -17,6 +18,9 @@ void interactive_shell() {
     int lineCount = 1;
     int functionReturn;
     TokenArray tokenArray;
+    int needBreak = F;
+    int gotReturn = F;
+    FunctionReturn funcReturn;
 
     strcpy(indicator, ">>>");
 
@@ -27,15 +31,18 @@ void interactive_shell() {
         tokenArray.tokens = malloc(TITIK_TOKEN_INIT_LENGTH * sizeof(Token));
         tokenArray.tokenCount = 0;
 
+        strcpy(userInput, "");
         printf("%s ", indicator);
         fgets(userInput, TITIK_CHAR_PER_LINE, stdin);
+
+        printf("%s\n", userInput);
 
         inputStr = malloc(lineCount * sizeof(char*));
         inputStr[0] = malloc(TITIK_CHAR_PER_LINE * sizeof(char));
         strcpy(inputStr[0], userInput);
 
-        generateToken(inputStr, lineCount, &tokenArray, "interactive_shell");
-
+        //generateToken(inputStr, lineCount, &tokenArray, "interactive_shell");
+        //parseToken(tokenArray, F, T, &needBreak, TITIK_MAIN_SCOPE_NAME, &funcReturn, &gotReturn);
         //dumpToken(tokenArray);
         free(tokenArray.tokens);
         free(inputStr[0]);
