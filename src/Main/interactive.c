818f5ffa5a3e810fae5c79fafa64d0134d0b1872
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2017-07-27 08:26:59 +0800

    generate token in interactive shell

--- src/Main/interactive.c
+++ src/Main/interactive.c
@@ -7,10 +7,16 @@
 #include "interactive.h"
 #include "info.h"
 #include "util.h"
+#include "lexer.h"
+#include "debug.h"
 
 void interactive_shell() {
     char userInput[TITIK_CHAR_PER_LINE];
     char indicator[TITIK_CHAR_PER_LINE];
+    char **inputStr;
+    int lineCount = 1;
+    int functionReturn;
+    TokenArray tokenArray;
 
     strcpy(indicator, ">>>");
 
@@ -18,7 +24,21 @@ void interactive_shell() {
     printf("To exit, press ^C\n");
 
     while(T) {
+        tokenArray.tokens = malloc(TITIK_TOKEN_INIT_LENGTH * sizeof(Token));
+        tokenArray.tokenCount = 0;
+
         printf("%s ", indicator);
         fgets(userInput, TITIK_CHAR_PER_LINE, stdin);
+
+        inputStr = malloc(lineCount * sizeof(char*));
+        inputStr[0] = malloc(TITIK_CHAR_PER_LINE * sizeof(char));
+        strcpy(inputStr[0], userInput);
+
+        generateToken(inputStr, lineCount, &tokenArray, "interactive_shell");
+
+        //dumpToken(tokenArray);
+        free(tokenArray.tokens);
+        free(inputStr[0]);
+        free(inputStr);
     }
 }
